a57a696e4ef4544dd6f172557130f144caaa232e
ye go <120290598+gogo2go007@users.noreply.github.com>
2023-01-21 16:27:46 +0900

    반응형 하는 중

---
+++ js/script.js
@@ -0,0 +1,300 @@
+// 플러그인 연결 위한 코드
+AOS.init();
+
+
+// 상단 메뉴
+const header = document.querySelector("#header");
+const gnb = document.querySelectorAll(".gnb > li");
+const subTit = document.querySelectorAll(".sub_tit li");
+const subBg = document.querySelector(".sub_tit_wrap");
+const sections = document.querySelectorAll("#section > .sections");
+// 섹션의 위치값을 담아줄 배열변수
+let secStart = [];
+
+window.addEventListener("scroll",function(){
+    // 현재 스크롤 위치를 담아줄 변수
+    let scTop = window.scrollY;
+
+    // 반복될 섹션의 범위를 지정하고 
+    // 몇번째 섹션의 시작점인지 secStart[i] 배열변수로 담아준다
+    for(let i = 0; i < sections.length; i++){
+        secStart[i] = sections[i].offsetTop;
+    }
+
+    // 현재의 스크롤이 섹션의 첫번째 보다 크거나 같고
+    // 섹션의 두번째 보다 작으면 실행한 코드
+    if(scTop >= secStart[0] && scTop < secStart[1]){
+        header.classList.remove("top");
+        header.classList.remove("on");
+        header.classList.remove("on2");
+    }
+    // 현재의 스크롤이 섹션의 두번째 보다 크거나 같고
+    // 섹션의 다섯번째 보다 작으면 실행한 코드
+    else if(scTop >= secStart[1] && scTop < secStart[3]){
+        header.classList.remove("top");
+        header.classList.add("on");
+        // subBg.classList.add("on");
+    }
+    else if(scTop >= secStart[3] && scTop < secStart[6]){
+        header.classList.remove("top");
+        header.classList.add("on2");
+        // subBg.classList.add("on");
+    }
+    else if(scTop >= secStart[6] && scTop < secStart[7]){
+        header.classList.remove("top");
+        header.classList.remove("on2");
+        header.classList.add("on");
+        // subBg.classList.add("on");
+    }
+    else{
+        header.classList.remove("top");
+        header.classList.remove("on");
+        header.classList.add("on2");
+        // subBg.classList.add("on");
+    }
+    // else if(scTop >= secStart[6]){
+    //     header.classList.remove("on");
+    //     header.classList.add("top");
+    // }
+});
+
+for(let i = 0; i < subTit.length; i++){
+    gnb[i].addEventListener("mouseenter",function(){
+        for(let j = 0; j < subTit.length; j++){
+            subTit[j].classList.remove("on");
+    
+        }
+        subTit[i].classList.add("on");
+    });
+}
+
+
+$(document).ready(function(){
+
+    // 슬라이드 영역
+    var len = $(".visual_pager li").length;
+    var wid = $(".visual").width();
+    var sss = 0;
+
+    $(".next").click(function(){
+        if(sss == len-1){
+            sss = 0;
+        }
+        else{
+            sss++;
+        }
+        slideMove();
+    });
+
+    $(".prev").click(function(){
+        if(sss == 0){
+            sss = len-1;
+        }
+        else{
+            sss--;
+        }
+        slideMove();
+    });
+
+    $(".vs_page_line li").click(function(){
+        sss = $(this).index();
+        slideMove();
+    });
+
+    var autoSlide = setInterval(function(){
+        if(sss == len-1){
+            sss = 0;
+        }
+        else{
+            sss++;
+        }
+        slideMove();
+    }, 3000);
+
+    $(".visual").mouseenter(function(){
+        clearInterval(autoSlide);
+    });
+
+    $(".visual").mouseleave(function(){
+        autoSlide = setInterval(function(){
+        if(sss == len-1){
+            sss = 0;
+        }
+        else{
+            sss++;
+        }
+        slideMove();
+    }, 3000);
+
+    });
+
+
+    function slideMove(){
+        $(".slide").stop().animate({"margin-left": -wid*sss});
+        $(".visual_pager li").removeClass("on");
+        $(".visual_pager li").eq(sss).addClass("on");
+        $(".vs_page_line li").removeClass("on");
+        $(".vs_page_line li").eq(sss).addClass("on");
+    }
+
+
+    // visual영역에서 scroll down 버튼 클릭시 다음 섹션으로 이동
+    $(".down_btn").click(function(){
+        $("html,body").stop().animate({scrollTop:1000},500)
+    });
+
+
+    // 미션 숫자 카운트 영역
+    var a = 0;
+    $(window).scroll(function(){
+        var mTop = $(".mission").offset().top-window.innerHeight;
+        if(a == 0 && $(window).scrollTop() > mTop){
+            $('.counter-value').each(function() {
+                var $this = $(this),
+                countTo = $this.attr('data-count');
+                $({countNum: $this.text()}).animate({
+                countNum: countTo},
+                {duration: 2000, easing: 'swing',
+                    step: function() {
+                        var num = numberWithCommas(Math.floor(this.countNum));
+                        $this.text(num);
+                    },
+                    complete: function() {
+
+                        var num = numberWithCommas(Math.floor(this.countNum));
+                        $this.text(num);
+                    }
+                });
+            });
+
+            $(".now_dt").each(function(){
+                var $this = $(this),
+                countTo = $this.attr('data-count');
+                $({countNum: $this.text()}).animate({
+                countNum: countTo},
+                {duration: 2000, easing: 'swing',
+                    step: function() {
+                        $this.text(Math.floor(this.countNum));
+                    },
+                    complete: function() {
+                        $this.text(this.countNum);
+                    }
+                });
+            });
+
+        a = 1;
+
+            // 세자리마다 콤마
+            function numberWithCommas(x) {
+                return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+            }
+        }
+    });
+
+
+    // 톤 크루 영역 bg 이미지가 스크롤 했을때 왼쪽에서 오른쪽으로 나타남
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 4000){
+            $(".crew_bg").css({"left":"0"});
+        }
+        else{
+
+        }
+    });
+
+
+
+    // 제품소개 아코디언 메뉴
+    $('.item_title').click(function(e){
+        e.preventDefault();
+        $('.item_box').stop().slideUp();
+
+        if ($(this).next('.item_box').is(':hidden')){
+            $(this).next('.item_box').stop().slideDown();
+            $(".item_title").children("span").css({"display":"none"});
+            $(this).children("span").css({"display":"inline-block"});
+            $(".item_title").children("a").css({"font-weight":"300"});
+            $(this).children("a").css({"font-weight":"400"});
+        } 
+        else{
+            $(this).next('.item_box').stop().slideUp();         
+        }
+        
+        
+
+       
+    });
+
+    // 제품소개 이미지 슬라이드
+    var item = 0;
+    var item_wid = $(".pro_list li").width();
+    var itme_len = $(".pro_list li").length;
+    $(".item_txt li").click(function(e){
+        e.preventDefault();
+
+        item = $(this).index();
+        $(".pro_list").stop().animate({"margin-left":-item_wid * item});
+        $(".pro_list li").removeClass("on");
+        $(".pro_list li").eq(item).addClass("on");
+    });
+
+
+    // top 버튼 
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 4800 && $(this).scrollTop() < 8400){
+            $(".scr_up").addClass("on");
+        }
+        else{
+            $(".scr_up").removeClass("on");
+        }
+        
+    });
+
+    $(".scr_up").click(function(){
+        $("html,body").stop().animate({scrollTop:0},500)
+    });
+
+    // 하단 footer 사업자정보 펼치고 접기
+    $(".f_inform_off").click(function(e){
+        e.preventDefault();
+        $(this).next(".f_txt").stop().slideDown();
+        $(this).removeClass("on");
+        $(".f_inform_on").addClass("on");
+    });
+
+    $(".f_inform_on").click(function(e){
+        e.preventDefault();
+        $(this).prev(".f_txt").stop().slideUp();
+        $(this).removeClass("on");
+        $(".f_inform_off").addClass("on");
+    })
+
+    // 새로고침 방지 위한 코드
+    function doNotReload(){
+        if( (event.ctrlKey == true && (event.keyCode == 78 || event.keyCode == 82)) || (event.keyCode == 116) ) {
+            event.keyCode = 0;
+            event.cancelBubble = true;
+            event.returnValue = false;
+        } 
+    }
+    document.onkeydown = doNotReload;
+});
+
+
+
+// 서브페이지 4_ 쇼핑 리스트 영역
+
+    // let createTag = "";
+
+    // productList.forEach(function(item,index){
+
+    //     createTag +=
+    //         `<div class="box">
+    //                 <a href="#"><img src="${item.imgSrc}"></a>
+    //                 <h3>${item.mainTitle}</h3>
+    //                 <p>${item.pice}</p>
+    //         </div>`
+    // });
+
+    // document.querySelector(".sub4_shop_list").innerHTML = createTag;
+
---
+++ js/shop_list.js
@@ -0,0 +1,82 @@
+let productList = [
+    {
+        imgSrc:"img/shop_thumb01.jpg",
+        mainTitle:"해남 무화과 시그니처 세트, *10% OFF",
+        pice:"81,900원"
+    },
+    {
+        imgSrc:"img/shop_thumb02.jpg",
+        mainTitle:"해남 무화과 초콜릿 A세트, *10% OFF",
+        pice:"28,800원"
+    },
+    {
+        imgSrc:"img/shop_thumb03.jpg",
+        mainTitle:"해남 무화과 오란다 B세트, *10% OFF",
+        pice:"17,100원"
+    },
+    {
+        imgSrc:"img/shop_thumb04.jpg",
+        mainTitle:"해남 무화과 수면팩 C세트, *10% OFF",
+        pice:"36,000원"
+    },
+    {
+        imgSrc:"img/shop_thumb05.jpg",
+        mainTitle:"BODY&HOME SET (바디오일+바디로션+캔들)",
+        pice:"49,900원"
+    },
+    {
+        imgSrc:"img/shop_thumb06.jpg",
+        mainTitle:"BEST HAIR SET (샴푸바 3종+트리트먼트바)",
+        pice:"35,500원"
+    },
+    {
+        imgSrc:"img/shop_thumb07.jpg",
+        mainTitle:"LIFE SET (고체치약 55정+200정+대나무 칫솔 2)",
+        pice:"30,000원"
+    },
+    {
+        imgSrc:"img/shop_thumb08.jpg",
+        mainTitle:"DISCOVERY KIT(미니바 4종, 여행용)",
+        pice:"19,200원"
+    },
+    {
+        imgSrc:"img/shop_thumb09.jpg",
+        mainTitle:"<3+1> 샴푸바 (모발보습케어) S19 바오밥 3개",
+        pice:"35,500원"
+    },
+    {
+        imgSrc:"img/shop_thumb10.jpg",
+        mainTitle:"<3+1> 샴푸바 (두피장벽강화) S21 검은콩 3개",
+        pice:"33,000원"
+    },
+    {
+        imgSrc:"img/shop_thumb11.jpg",
+        mainTitle:"<3+1> 샴푸바 (지성/각질케어) S18 다시마 3개",
+        pice:"33,000원"
+    },
+    {
+        imgSrc:"img/shop_thumb12.jpg",
+        mainTitle:"샴푸바 (두피쿨링) V1 블루비오틴",
+        pice:"18,000원"
+    },
+    {
+        imgSrc:"img/shop_thumb13.jpg",
+        mainTitle:"두피 토닉 + 샴푸바 V1, 블루비오틴 SET",
+        pice:"38,000원"
+    },
+    {
+        imgSrc:"img/shop_thumb14.jpg",
+        mainTitle:"고체향수 NO.28 CUT GRASS 12g",
+        pice:"34,000원"
+    },
+    {
+        imgSrc:"img/shop_thumb15.jpg",
+        mainTitle:"샌달우드 괄사",
+        pice:"27,000원"
+    },
+    {
+        imgSrc:"img/shop_thumb16.jpg",
+        mainTitle:"바디 오일 회상(回想) 100ml",
+        pice:"28,000원"
+    }
+];
